8c1417f42dbb89bcf6733f8e1cbe4bd56924837e
kenta0603 <noda.5552.k@gmail.com>
2022-10-27 06:11:27 +0900

    blog1027

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GogleAuthProvider } from 'firebase/auth';
+import { getFirestore} from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDWzowZkHRphuTTwkbnDVBcOxwF8dZZnZQ",
+  authDomain: "blog-1fa7e.firebaseapp.com",
+  projectId: "blog-1fa7e",
+  storageBucket: "blog-1fa7e.appspot.com",
+  messagingSenderId: "126695946715",
+  appId: "1:126695946715:web:9a70f2b1f7e2c25f16cffe"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GogleAuthProvider();
+const db = getFirestore(app);
+
+export { auth, provider, db };
